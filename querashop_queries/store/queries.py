869c7f374b92ff061237a29641376fc2bd921efe
hannane sharifi <127097038+hananesharifi@users.noreply.github.com>
2023-08-09 11:38:05 +0330

    Update queries.py

--- querashop_queries/store/queries.py
+++ querashop_queries/store/queries.py
@@ -1,43 +1,58 @@
-from django.db.models import Avg, Count, Sum
+from django.db.models import Sum, Avg, Count
 from datetime import datetime, timedelta
-from store.models import Employee ,Company ,Customer ,Product ,Order
+
+from store.models import Company, Product, Employee, Customer, Order
+
 
 def young_employees(job: str):
     return Employee.objects.filter(age__lt=30, job=job)
 
+
 def cheap_products():
-    avgPrice = Product.objects.aaggregate(Avg('price'))['price__avg']
-    cheap = Product.objects.filter(price__lt=avgPrice).order_by('price')
-    return cheap
+    average_price = Product.objects.aggregate(avg_price=Avg('price'))['avg_price']
+    cheap_products = Product.objects.filter(price__lt=average_price).order_by('price')
+    return cheap_products.values_list('name', flat=True)
 
 def products_sold_by_companies():
-    companies = Company.objects.prefetch_related('product_set')
+    companies = Company.objects.all()
     result = []
+
     for company in companies:
-        products_sold = [(product.name, product.sold) for product in company.product_set.all()]
-        result.append((company.name, products_sold))
+        sold_products = company.product_set.aggregate(total_sold=Sum('sold'))['total_sold']
+        result.append((company.name, sold_products))
+    
     return result
 
-def sum_of_income(start_date: str, end_date: str):
-    start_date_obj = datetime.strptime(start_date ,'%Y-%m-%d')
-    end_date_obj = datetime.strptime(end_date ,'%Y-%m-%d')
-    
-    income_sum = Order.objects.filter(date__gte=start_date_obj, date__lte=end_date_obj).aggregate(Sum('price'))['price__sum']
 
-    return income_sum
+def sum_of_income(start_date: str, end_date: str):
+    start_date = datetime.strptime(start_date, '%Y-%m-%d')
+    end_date = datetime.strptime(end_date, '%Y-%m-%d')
+    return Order.objects.filter(time__gte=start_date, time__lte=end_date).aggregate(income=Sum('price'))['income']
+
 
 def good_customers():
-    # Calculate the date range for the past month
-    today = datetime.now().date()
-    last_month_start = (today - timedelta(days=30)).replace(day=1)
+    last_month_start = datetime.now() - timedelta(days=30)
+    valuable_customers = Customer.objects.filter(level='G',order__time__gte=last_month_start).annotate(total_orders=Count('order')).filter(total_orders__gt=10)
     
-    # Retrieve customers with more than 10 purchases in the past month
-    return Customer.objects.annotate(purchase_count=Count('purchase')).filter(
-        purchase__purchase_date__gte=last_month_start,
-        purchase_count__gt=10
-    )
+    result = [(customer.name, customer.phone) for customer in valuable_customers]
+    return result
+
 
 def nonprofitable_companies():
-    return Company.objects.annotate(
-        product_count=Count('product', filter=models.Q(product__price__lt=100))
-    ).filter(product_count__gte=4)
+    companies = Company.objects.all()
+    result = []
+
+    for company in companies:
+        low_sold_products = 0
+
+        for product in company.product_set.all():
+            if product.sold < 100:
+                low_sold_products += 1
+
+        if low_sold_products >= 4:
+            result.append(company.name)
+
+    return result
+
+
+
